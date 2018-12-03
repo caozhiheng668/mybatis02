3af3620fcca9e9ef99ee02996d5fd4bfd0d53f86
itcast <bos@itcast.cn>
2018-12-03 21:08:46 +0800

    修改了

---
+++ src/main/java/com/itheima/dao/YY.java
@@ -0,0 +1,14 @@
+package com.itheima.dao;
+
+/**
+ * @BelongsProject: mybatis02
+ * @BelongsPackage: com.itheima.dao
+ * @Author: zhiheng
+ * @CreateTime: 2018-12-03 21:06
+ * @Description: ${Description}
+ */
+public class YY {
+    public static void main(String[] args) {
+
+    }
+}
